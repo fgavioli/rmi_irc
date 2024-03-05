1773c692205ef4e2a7a8357237bdcdfb3bf33a74
Federico Gavioli <224833@studenti.unimore.it>
2024-03-05 18:27:36 +0100

    signatureVerifier: check if nonce is null in verifySignature

--- src/server/SignatureVerifier.java
+++ src/server/SignatureVerifier.java
@@ -15,6 +15,7 @@ public class SignatureVerifier {
     public void addSignature(String username, PublicKey publicKey) throws NoSuchAlgorithmException, InvalidKeyException {
         Signature signature = Signature.getInstance("SHA256withRSA");
         signature.initVerify(publicKey);
+        clientKeys.put(username, signature);
     }
 
     public void removeSignature(String username) {
@@ -22,6 +23,8 @@ public class SignatureVerifier {
     }
 
     public boolean verifySignature(String username, byte[] message, byte[] nonce, byte[] signedFingerprint) {
+        if (nonce == null)
+            nonce = new byte[0];
         byte[] byteMessage = new byte[message.length + nonce.length];
         ByteBuffer bb = ByteBuffer.wrap(byteMessage);
         bb.put(message);
