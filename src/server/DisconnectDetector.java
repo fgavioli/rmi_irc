d2a3e467a654fcdc10124da5a3c38dcc1811a1d9
Federico Gavioli <224833@studenti.unimore.it>
2024-03-06 18:31:23 +0100

    DisconnectDetector: implement disconnect check in run()

--- src/server/DisconnectDetector.java
+++ src/server/DisconnectDetector.java
@@ -1,9 +1,15 @@
 package server;
 
+import client.IRCClientInterface;
+
+import java.lang.reflect.Array;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Objects;
+import java.util.Vector;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class DisconnectDetector implements Runnable {
     private IRCServer server;
@@ -11,10 +17,47 @@ public class DisconnectDetector implements Runnable {
     public DisconnectDetector(IRCServer server) {
         this.server = server;
     }
+
+    private void checkClients(ConcurrentHashMap<String, IRCClientInterface> clients) {
+        ArrayList<String> clientsToCheck = new ArrayList<>(clients.keySet());
+        while (!clientsToCheck.isEmpty()) {
+            ArrayList<String> checkedClients = new ArrayList<>();
+            String lastUser = "";
+            try {
+                for (String user : clientsToCheck) {
+                    lastUser = user;
+                    checkedClients.add(user);
+                    clients.get(user).getUsername();
+                }
+            } catch (RemoteException e) {
+                // remove client from lobby
+                server.removeClient(lastUser);
+            }
+            // remove all checked clients from the list and restart the check with the remaining ones
+            clientsToCheck.removeAll(checkedClients);
+        }
+    }
+
     @Override
     public void run() {
-        // TODO: RESTRUCTURE THIS FOLLOWING SENDMESSAGE
-        ArrayList<String> allClients;
+
+        // Check disconnected users in lobby
+        ConcurrentHashMap<String, IRCClientInterface> clients = new ConcurrentHashMap<>(server.getClientsInLobby());
+        checkClients(clients);
+        clients.clear();
+
+        // Check disconnected users in channels
+        ArrayList<Channel> channels = new ArrayList<>(server.getChannels());
+        for (Channel c : channels)
+            clients.putAll(c.getClients());
+        checkClients(clients);
+        clients.clear();
+
+        // Check private chats
+//        ArrayList<Channel> privateChats = new ArrayList<>(server.getPrivateChats());
+
+
+// CHECK PRIVATE CHATS AND DESTROY IF ONE OF THE CLIENTS IS OFFLINE
 //        System.out.print("Lobby: ");
 //        for (String user : server.getClientsInLobby().keySet()) {
 //            try { // call getUsername to check if the client is still alive
