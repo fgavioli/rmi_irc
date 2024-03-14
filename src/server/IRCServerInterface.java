657224cdc85d74eb06df489260a6c479bc79a95d
Federico Gavioli <224833@studenti.unimore.it>
2024-03-14 17:24:54 +0100

    IRCServerInterface: refactor getChannelNames

--- src/server/IRCServerInterface.java
+++ src/server/IRCServerInterface.java
@@ -9,7 +9,7 @@ public interface IRCServerInterface extends Remote {
     int connect(String username, PublicKey publicKey, byte[] signedFingerprint) throws RemoteException;
     String getGreeting() throws RemoteException;
     ArrayList<String> getUsers() throws RemoteException;
-    ArrayList<String> getChannelNames() throws RemoteException;
+    ArrayList<String> getChannelDescriptions() throws RemoteException;
     int joinChannel(String username, String channelName, byte[] signedFingerprint) throws RemoteException;
     int leaveChannel(String username, String channelName, byte[] signedFingerprint) throws RemoteException;
     int joinPrivateChat(String username, String targetUsername, byte[] signedFingerprint) throws RemoteException;
