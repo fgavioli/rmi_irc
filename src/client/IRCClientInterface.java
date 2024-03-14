8e52b6ca66af04ad8dbf4e3f3456b639d66d735a
Federico Gavioli <224833@studenti.unimore.it>
2024-03-14 17:59:14 +0100

    IRCClientInterface: remove unused imports

--- src/client/IRCClientInterface.java
+++ src/client/IRCClientInterface.java
@@ -3,7 +3,6 @@ package client;
 import java.io.IOException;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
-import java.security.PublicKey;
 
 public interface IRCClientInterface extends Remote {
     String getUsername() throws RemoteException;
